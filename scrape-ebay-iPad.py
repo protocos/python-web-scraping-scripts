5a255c77eff42458937e8be0bca37aa1af7a1490
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2017-10-18 16:28:45 -0500

    initial commit

---
+++ scrape-ebay-iPad.py
@@ -0,0 +1,35 @@
+import time
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+scrape_url = "https://www.ebay.com/sch/i.html?_udlo=100&_udhi=270&_mPrRngCbx=1&_from=R40&_sacat=0&_nkw=ipad+5th+generation+-mini+-broken+-air+space+gray&rt=nc&LH_BIN=1"
+
+notification_endpoint = "https://maker.ifttt.com/trigger/notification/with/key/VzmWoFF515H4lf0MNNVyo?value1=New Matched Result!&value2="
+
+minutes = 60
+sleep_duration = 15 * minutes
+hrefs = []
+
+def poll_ebay_for_iPad():
+    r = requests.get(scrape_url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    print eval('soup.find_all("li", {"class":"sresult"})')
+    for item in soup.find_all("li", {"class":"sresult"}):
+        a = item.find_all("a")[0]
+        href = a.get("href")
+
+        if href not in hrefs:
+            requests.get(notification_endpoint + href)
+            hrefs.append(href)
+
+try:
+    while True:
+        poll_ebay_for_iPad()
+        time.sleep(minutes)
+except KeyboardInterrupt:
+    print("Quitting the program.")
+except:
+    print("Unexpected error: "+sys.exc_info()[0])
+    raise
