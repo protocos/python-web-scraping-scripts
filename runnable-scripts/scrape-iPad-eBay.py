fcf252fa4958a47bc1fb9e56d7ddd4a917238b86
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2017-10-27 15:51:33 -0500

    updating link for eBay

--- runnable-scripts/scrape-iPad-eBay.py
+++ runnable-scripts/scrape-iPad-eBay.py
@@ -4,7 +4,7 @@ import reusables
 
 #soup = reusables.scrape("https://www.ebay.com/sch/i.html?_udlo=100&_udhi=240&LH_BIN=1&_mPrRngCbx=1&_from=R40&_sacat=0&_nkw=ipad%205th%20generation%20-mini%20-broken%20-air%20space%20gray%20-dent&rt=nc")
 #soup = reusables.scrape("https://www.ebay.com/sch/i.html?_udlo=100&_udhi=190&LH_BIN=1&_mPrRngCbx=1&_from=R40&_sacat=0&_nkw=ipad%20air%202%20new%20-mini%20-case%20-screen%20-keyboard%20-cover%20-hardcase%20-stand%20-sleeve%20-adapter%20-battery%20-frame&rt=nc")
-soup = reusables.scrape("ipad 5th generation -aXtion -keyboard -mount -digitizer -replacement -screen -broken")
+soup = reusables.scrape("https://www.ebay.com/sch/i.html?_udlo=116&_udhi=180&_mPrRngCbx=1&LH_BIN=1&_from=R40&_sacat=0&_nkw=ipad%205th%20generation%20-aXtion%20-keyboard%20-mount%20-digitizer%20-replacement%20-screen%20-broken&rt=nc")
 
 for item in soup.find_all("li", {"class":"sresult"}):
     a = item.find_all("a")[0]
