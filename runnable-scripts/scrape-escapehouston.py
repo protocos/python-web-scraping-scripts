c13485da9c3a971908aafed0ff2d0561160f94cb
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2017-10-30 22:53:47 -0500

    Updated script to work

--- runnable-scripts/scrape-escapehouston.py
+++ runnable-scripts/scrape-escapehouston.py
@@ -6,7 +6,7 @@ soup = reusables.scrape("https://escapehouston.com/")
 
 for item in soup.find_all("div", {"class":"post"}):
     a = item.find("h2", {"class":"excerpt-title"}).find("a")
-    title = a.contents
+    title = a.getText()
     href = a.get("href")
-
+    
     reusables.check_href_and_send_notification("New Flight Deal!", title, href)
