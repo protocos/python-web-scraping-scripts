451bb67ca420f8de24775a9c6fa455a95a22924a
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2017-10-30 22:57:34 -0500

    removed blank line

--- runnable-scripts/scrape-escapehouston.py
+++ runnable-scripts/scrape-escapehouston.py
@@ -8,5 +8,5 @@ for item in soup.find_all("div", {"class":"post"}):
     a = item.find("h2", {"class":"excerpt-title"}).find("a")
     title = a.getText()
     href = a.get("href")
-    
+
     reusables.check_href_and_send_notification("New Flight Deal!", title, href)
