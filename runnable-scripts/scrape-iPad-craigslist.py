6244c44ec06bc4c0b78ae845d5aa22782a997d9b
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2017-11-25 15:12:02 -0600

    reducing price for ipad air 2 on craigslist to 200 or lower

--- runnable-scripts/scrape-iPad-craigslist.py
+++ runnable-scripts/scrape-iPad-craigslist.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('..')
 import reusables
 
-soup = reusables.scrape("https://houston.craigslist.org/search/sss?query=ipad+air+2&sort=rel&srchType=T&hasPic=1&search_distance=50&postal=77081&min_price=100&max_price=240")
+soup = reusables.scrape("https://houston.craigslist.org/search/sss?query=ipad+air+2&sort=rel&srchType=T&hasPic=1&search_distance=50&postal=77081&min_price=100&max_price=200")
 
 for item in soup.find_all("li", {"class":"result-row"}):
     a = item.find_all("a")[0]
