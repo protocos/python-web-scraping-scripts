b91aebd845294a4faf9d62621500ba88be4d9390
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2017-12-07 11:34:56 -0600

    Updating R350 wheel search to not include an upper price limit

--- runnable-scripts/scrape-R350-wheel-eBay.py
+++ runnable-scripts/scrape-R350-wheel-eBay.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('..')
 import reusables
 
-soup = reusables.scrape("https://www.ebay.com/sch/i.html?_udhi=150&_mPrRngCbx=1&LH_BIN=1&_from=R40&_sacat=0&_nkw=SVE%20R350%20-mercedes&rt=nc&_udlo")
+soup = reusables.scrape("https://www.ebay.com/sch/i.html?_mPrRngCbx=1&LH_BIN=1&_from=R40&_sacat=0&_nkw=SVE%20R350%20-mercedes&_trksid=p2045573.m1684.l5985")
 
 for item in soup.find_all("li", {"class":"sresult"}):
     a = item.find_all("a")[0]
