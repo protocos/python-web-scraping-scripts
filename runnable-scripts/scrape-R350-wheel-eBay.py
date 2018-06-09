9e3e81f36f2e582eceb255b64b4345c2ef606344
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2018-06-09 12:53:41 -0500

    updated thresholds on R350 wheel

--- runnable-scripts/scrape-R350-wheel-eBay.py
+++ runnable-scripts/scrape-R350-wheel-eBay.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('..')
 import reusables
 
-soup = reusables.scrape("https://www.ebay.com/sch/i.html?_mPrRngCbx=1&LH_BIN=1&_from=R40&_sacat=0&_nkw=SVE%20R350%20-mercedes&_trksid=p2045573.m1684.l5985")
+soup = reusables.scrape("https://www.ebay.com/sch/i.html?_mPrRngCbx=1&LH_BIN=1&_from=R40&_sacat=0&_fosrp=1&_nkw=SVE%20R350%20-mercedes&_dcat=6030&rt=nc&_udlo=100&_udhi=500")
 
 for item in soup.find_all("li", {"class":"sresult"}):
     a = item.find_all("a")[0]
