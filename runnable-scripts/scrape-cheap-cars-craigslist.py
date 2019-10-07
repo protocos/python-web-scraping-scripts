dfae044b2c44e3ad44af2d184e2adf7931c1f5b4
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2019-10-07 18:48:06 -0500

    Updating with odometer, year, make, model, and other refactorings

--- runnable-scripts/scrape-cheap-cars-craigslist.py
+++ runnable-scripts/scrape-cheap-cars-craigslist.py
@@ -11,27 +11,28 @@ def scrape(soup, car_type):
         reusables.check_href_and_send_notification("New "+car_type+" Found!", href, href)
 
 
-def price_str(min_price, max_price):
-    return "&min_price="+str(min_price)+"&max_price="+str(max_price)
+def parameters(min_price, max_price, max_miles, year, make, model):
+    return "&min_price="+str(min_price)+"&max_price="+str(max_price)+"&min_auto_year="+str(year)+"&max_auto_year="+str(year)+"&min_auto_miles="+str(1000)+"&max_auto_miles="+str(max_miles)+"&max_auto_miles="+str(max_miles)+"&auto_make_model="+str(make)+"+"+str(model)
 
 
-price_min = 1300
-price_max = 7500
-scrapTerms = "+-Credit+-Wheels+-Tires+-Engine+-Motor+-Special+-Low+-Down+-Finance+-Buy"
-searchQuery = scrapTerms+"&sort=rel&srchType=T&hasPic=1&postedToday=1&bundleDuplicates=1"
-price = "&min_price="+str(price_min)+"&max_price="+str(price_max)
+base_query = "https://houston.craigslist.org/search/sss?query=-Credit+-Wheels+-Tires+-Engine+-Motor+-Special+-Low+-Down+-Finance+-Buy&sort=rel&srchType=T&hasPic=1&bundleDuplicates=1&postedToday=1"
+min_price = 1300
+max_price = 7500
+max_mileage = 150000
 
-Camry2011 = reusables.scrape("https://houston.craigslist.org/search/sss?query=2011+Camry"+searchQuery+price)
+
+reusables.clear_hrefs()
+Camry2011 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2011, "Toyota", "Camry"))
 scrape(Camry2011, "2011 Camry")
 
-Corolla2012 = reusables.scrape("https://houston.craigslist.org/search/sss?query=2012+Corolla"+searchQuery+price)
+Corolla2012 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2012, "Toyota", "Carolla"))
 scrape(Corolla2012, "2012 Corolla")
 
-Fusion2012 = reusables.scrape("https://houston.craigslist.org/search/sss?query=2012+Fusion"+searchQuery+price)
+Fusion2012 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2012, "Ford", "Fusion"))
 scrape(Fusion2012, "2012 Fusion")
 
-Civic2013 = reusables.scrape("https://houston.craigslist.org/search/sss?query=2013+Civic"+searchQuery+price)
+Civic2013 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2013, "Honda", "Civic"))
 scrape(Civic2013, "2013 Civic")
 
-Accord2012 = reusables.scrape("https://houston.craigslist.org/search/sss?query=2012+Accord"+searchQuery+price)
+Accord2012 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2012, "Honda", "Accord"))
 scrape(Accord2012, "2012 Accord")
