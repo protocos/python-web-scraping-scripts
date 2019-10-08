5cf0cefd6424ae6b6a33fdfc86cfaa4dc0021dd6
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2019-10-07 20:07:19 -0500

    final scrape with pictures

--- runnable-scripts/scrape-cheap-cars-craigslist.py
+++ runnable-scripts/scrape-cheap-cars-craigslist.py
@@ -3,35 +3,32 @@ sys.path.append('..')
 import reusables
 
 
-def scrape(soup, car_type):
+def scrape(car_type, query):
+    soup = reusables.scrape(query)
     for item in soup.find_all("li", {"class": "result-row"}):
         a = item.find_all("a")[0]
         href = a.get("href")
+        price = item.find("span", {"class": "result-price"}).text
+        post = reusables.scrape(href)
+        img = post.find("img")["src"]
 
-        reusables.check_href_and_send_notification("New "+car_type+" Found!", href, href)
+        reusables.check_value3_and_send_notification('news_with_image',
+                                                     "New "+car_type+" Found for "+price+"!",
+                                                     "%23[Original Post]("+href+")\n\n---\n\n%23%23%23[Original Search]("+query+")",
+                                                     img)
 
 
 def parameters(min_price, max_price, max_miles, year, make, model):
-    return "&min_price="+str(min_price)+"&max_price="+str(max_price)+"&min_auto_year="+str(year)+"&max_auto_year="+str(year)+"&min_auto_miles="+str(1000)+"&max_auto_miles="+str(max_miles)+"&max_auto_miles="+str(max_miles)+"&auto_make_model="+str(make)+"+"+str(model)
+    return "&min_price="+str(min_price)+"&max_price="+str(max_price)+"&min_auto_year="+str(year)+"&max_auto_year="+str(year)+"&min_auto_miles="+str(1000)+"&max_auto_miles="+str(max_miles)+"&auto_make_model="+str(make)+"+"+str(model)
 
 
-base_query = "https://houston.craigslist.org/search/sss?query=-Credit+-Wheels+-Tires+-Engine+-Motor+-Special+-Low+-Down+-Finance+-Buy&sort=rel&srchType=T&hasPic=1&bundleDuplicates=1&postedToday=1"
+base_query = "https://houston.craigslist.org/search/sss?query=-Credit+-Wheels+-Tires+-Engine+-Motor+-Special+-Low+-Down+-Finance+-Buy&sort=rel&srchType=T&hasPic=1&bundleDuplicates=1"
 min_price = 1300
 max_price = 7500
 max_mileage = 150000
 
-
-Camry2011 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2011, "Toyota", "Camry"))
-scrape(Camry2011, "2011 Camry")
-
-Corolla2012 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2012, "Toyota", "Corolla"))
-scrape(Corolla2012, "2012 Corolla")
-
-Fusion2012 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2012, "Ford", "Fusion"))
-scrape(Fusion2012, "2012 Fusion")
-
-Civic2013 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2013, "Honda", "Civic"))
-scrape(Civic2013, "2013 Civic")
-
-Accord2012 = reusables.scrape(base_query+parameters(min_price, max_price, max_mileage, 2012, "Honda", "Accord"))
-scrape(Accord2012, "2012 Accord")
+scrape("2011 Camry", base_query+parameters(min_price, max_price, max_mileage, 2011, "Toyota", "Camry"))
+scrape("2012 Corolla", base_query+parameters(min_price, max_price, max_mileage, 2012, "Toyota", "Corolla"))
+scrape("2012 Fusion", base_query+parameters(min_price, max_price, max_mileage, 2012, "Ford", "Fusion"))
+scrape("2013 Civic", base_query+parameters(min_price, max_price, max_mileage, 2013, "Honda", "Civic"))
+scrape("2012 Accord", base_query+parameters(min_price, max_price, max_mileage, 2012, "Honda", "Accord"))
--- runnable-scripts/scrape-reddit-travel.py
+++ runnable-scripts/scrape-reddit-travel.py
@@ -18,14 +18,7 @@ reddit = praw.Reddit(client_id=auth[0],
                      password=auth[4])
 
 
-def check_href_and_send_notification(value1, value2, value3):
-    if value3 not in reusables.get_hrefs():
-        requests.get("https://maker.ifttt.com/trigger/guess_where/with/key/VzmWoFF515H4lf0MNNVyo?value1=" + value1 + "&value2=" + value2 + "&value3=" + value3,
-                     timeout=30)
-        reusables.add_href(value3)
-
-
 subreddit = reddit.subreddit('travel')
 for submission in subreddit.top(limit=10, time_filter='month'):
     if submission.url.endswith('.jpg'):
-        check_href_and_send_notification("Guess where?", "%23[Original Post](https://www.reddit.com/r/travel/comments/"+submission.id+")\n\n---\n\n%23%23%23[IFTTT Recipe](https://ifttt.com/applets/105126139d/edit)", submission.url)
+        reusables.check_value3_and_send_notification('guess_where', "Guess where?", "%23[Original Post](https://www.reddit.com/r/travel/comments/"+submission.id+")\n\n---\n\n%23%23%23[IFTTT Recipe](https://ifttt.com/applets/105126139d/edit)", submission.url)
