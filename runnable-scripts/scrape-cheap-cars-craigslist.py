8d9959afafa357df6822705089009c629f7addcc
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2019-10-25 15:05:16 -0500

    Adjusting max prices

--- runnable-scripts/scrape-cheap-cars-craigslist.py
+++ runnable-scripts/scrape-cheap-cars-craigslist.py
@@ -32,12 +32,12 @@ def parameters(min_price, max_price, max_miles, min_year, max_year, make, model)
 
 base_query = "https://houston.craigslist.org/search/sss?query=-Credit+-Wheels+-Tires+-Engine+-Motor+-Special+-Low+-Down+-Finance+-Buy&sort=rel&srchType=T&hasPic=1&bundleDuplicates=1&auto_title_status=1"
 min_price = 3100
-max_price = 7800
+max_price = 7600
 max_mileage = 150000
 
-scrape("2011-2012 Camry", base_query+parameters(min_price, max_price, max_mileage, 2011, 2012, "Toyota", "Camry"))
+scrape("2011-2012 Camry", base_query+parameters(min_price, 7100, max_mileage, 2011, 2012, "Toyota", "Camry"))
 scrape("2011-2012 IS250", base_query+parameters(min_price, max_price, max_mileage, 2011, 2019, "Lexus", "IS250"))
 scrape("2012 Corolla", base_query+parameters(min_price, max_price, max_mileage, 2012, 2012, "Toyota", "Corolla"))
-scrape("2012 Fusion", base_query+parameters(min_price, max_price, max_mileage, 2012, 2012, "Ford", "Fusion"))
+scrape("2012 Fusion", base_query+parameters(min_price, 6300, max_mileage, 2012, 2012, "Ford", "Fusion"))
 scrape("2013 Civic", base_query+parameters(min_price, max_price, max_mileage, 2013, 2013, "Honda", "Civic"))
-scrape("2012 Accord", base_query+parameters(min_price, max_price, max_mileage, 2012, 2012, "Honda", "Accord"))
+scrape("2012 Accord", base_query+parameters(min_price, 7400, max_mileage, 2012, 2012, "Honda", "Accord"))
