1e9b99bf42638b4accd1b50269f5d0c3f9c71559
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2019-10-25 15:06:09 -0500

    Reducing max mileage

--- runnable-scripts/scrape-cheap-cars-craigslist.py
+++ runnable-scripts/scrape-cheap-cars-craigslist.py
@@ -33,7 +33,7 @@ def parameters(min_price, max_price, max_miles, min_year, max_year, make, model)
 base_query = "https://houston.craigslist.org/search/sss?query=-Credit+-Wheels+-Tires+-Engine+-Motor+-Special+-Low+-Down+-Finance+-Buy&sort=rel&srchType=T&hasPic=1&bundleDuplicates=1&auto_title_status=1"
 min_price = 3100
 max_price = 7600
-max_mileage = 150000
+max_mileage = 130000
 
 scrape("2011-2012 Camry", base_query+parameters(min_price, 7100, max_mileage, 2011, 2012, "Toyota", "Camry"))
 scrape("2011-2012 IS250", base_query+parameters(min_price, max_price, max_mileage, 2011, 2019, "Lexus", "IS250"))
