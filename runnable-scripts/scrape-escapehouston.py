39a4a6af3503e066cf70fd723c0fa41c618c53a5
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2020-03-11 23:18:38 -0500

    Adding split logic for better cards and defaulting to old way of notifications

--- runnable-scripts/scrape-escapehouston.py
+++ runnable-scripts/scrape-escapehouston.py
@@ -1,4 +1,5 @@
 import sys
+import re
 sys.path.append('..')
 import reusables
 
@@ -11,5 +12,32 @@ for item in soup.find_all("div", {"class":"post"}):
 
     p = item.find("div", {"class":"excerpt-content"}).find("article").find('p')
     desc = p.getText()
+    pattern = '(.+) (has|have) (.+) flights from (.+?) to (.+) for (\$.+?),? (.+)\. Flights (.+?)\. (.+)'
+    matches = re.search(pattern, desc)
+    # print('Description:',desc)
+    # print('Matches:')
+    if matches:
+        # for group in matches.groups():
+        #     print(group)
+        airline = matches.group(1)
+        type_of_flight = matches.group(3)
+        origin = matches.group(4)
+        destination = matches.group(5)
+        cost = matches.group(6)
+        connection = matches.group(7)
+        timeframe = matches.group(8)
+        additional_info_separated_by_sentences = matches.group(9)
 
+        cardTitle = 'Flight :: ' + destination + ' for ' + cost + ' ' + timeframe
+        cardDescription = ("Origin: "+origin+"\n"
+                           "Destination: "+destination+"\n"
+                           "Cost: "+cost+"\n"
+                           "Type: "+type_of_flight+", "+connection+"\n"
+                           "Timeframe: "+timeframe+"\n"
+                           "\n"
+                           ""+additional_info_separated_by_sentences+"\n")
+        print("cardTitle",cardTitle)
+        print("cardDescription",cardDescription)
+    else:
+        print(desc)
     reusables.check_href_and_send_notification("New Flight Deal!", desc, href)
